c05c1fd7154aeb738baa8441a1cebe80f50a5978
Roni Wellington <roniwellington@gmail.com>
2022-12-31 10:30:11 -0300

    em andamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-'use strict'
+'use strict';
+
+
 
 /**
  * #PRELOADING
  */
+
 const loadElement = document.querySelector("[data-preloader]");
 
-window.addEventListener("load", function() {
-    loadElement.classList.add("loaded")
+window.addEventListener("load", function () {
+  loadElement.classList.add("loaded");
 });
+
+
 
 /**
  * #MOBILE NAVBAR TOGGLE
@@ -17,11 +22,13 @@ const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
 const toggleNavbar = function () {
-    navbar.classList.toggle("active");
-    navToggler.classList.toggle("active")
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
 }
 
 navToggler.addEventListener("click", toggleNavbar);
+
+
 
 /**
  *  #HEADER
@@ -30,15 +37,17 @@ navToggler.addEventListener("click", toggleNavbar);
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-go-top-btn]");
 
-window.addEventListener("scroll", function (){
-    if(window.scrollY >= 100){
-        header.classList.add("active");
-        backTopBtn.classList.add("active")
-    } else {
-        header.classList.remove("active");
-        backTopBtn.classList.remove("active");
-    }
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
 });
+
+
 
 /**
  * #SCROLL REVEAL
@@ -46,14 +55,14 @@ window.addEventListener("scroll", function (){
 
 const revealElements = document.querySelectorAll("[data-reveal]");
 
-const scrollReveal = function() {
-    for(let i = 0, x = revealElements.length; i < x; i++){
-        if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2){
-            revealElements[i].classList.add("revealed");
-        } else {
-            revealElements[i].classList.remove("revealed")
-        }
+const scrollReveal = function () {
+  for (let i = 0, x = revealElements.length; i < x; i++) {
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
     }
+  }
 }
 
 window.addEventListener("scroll", scrollReveal);
