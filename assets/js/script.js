45ee2bb9a8fc778f1364f179f373cb8b25540f0f
Roni Wellington <roniwellington@gmail.com>
2022-12-31 07:37:51 -0300

    menu mobile

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+ * #PRELOADING
+ */
+const loadElement = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function() {
+    loadElement.classList.add("loaded")
+});
+
+/**
+ * #MOBILE NAVBAR TOGGLE
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active")
+}
+
+navToggler.addEventListener("click", toggleNavbar);
